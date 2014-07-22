992e7d386443100d856b51103a20852204c63498
Bradford Knowlton <brad@ucanbonline.net>
2014-07-22 15:59:14 -0600

    initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: DM Custom Content
+Plugin URI: http://designmissoula.com/
+Description: Used by millions, to make WordPress Better. Uses simple shortcode to switch between blogs on a multi blog network.
+Version: 3.0.2
+Author: Bradford Knowlton
+Author URI: http://bradknowlton.com
+License: GPLv2 or later
+GitHub Plugin URI: https://github.com/DesignMissoula/DM-custom-content
+GitHub Branch:     designmissoula.com
+*/
