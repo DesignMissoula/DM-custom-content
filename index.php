fa547b3ed7b20ded4edee1fb3348c1fe7d897967
Bradford Knowlton <brad@ucanbonline.net>
2014-07-22 16:02:38 -0600

    version bump

--- index.php
+++ index.php
@@ -3,10 +3,12 @@
 Plugin Name: DM Custom Content
 Plugin URI: http://designmissoula.com/
 Description: Used by millions, to make WordPress Better. Uses simple shortcode to switch between blogs on a multi blog network.
-Version: 3.0.2
+Version: 3.0.8
 Author: Bradford Knowlton
 Author URI: http://bradknowlton.com
 License: GPLv2 or later
 GitHub Plugin URI: https://github.com/DesignMissoula/DM-custom-content
 GitHub Branch:     designmissoula.com
 */
+
+// updated
